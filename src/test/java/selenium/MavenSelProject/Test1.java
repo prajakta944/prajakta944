8396d53679474d4c6a5cae52142e2fd6cdef5f30
prajakta <surve.prajakta915@gmail.com>
2022-02-16 14:41:47 +0530

    Changes in Test1

--- src/test/java/selenium/MavenSelProject/Test1.java
+++ src/test/java/selenium/MavenSelProject/Test1.java
@@ -4,17 +4,22 @@ import org.testng.annotations.Test;
 
 public class Test1 {
 
-	@Test//(dependsOnMethods = { "Test13" })
+	@Test // (dependsOnMethods = { "Test13" })
 	public void Test11() {
 		System.out.println("Test11");
 	}
 
+	//GIT new line add and commit the same
+	// GIT new line add and commit the same
+	// GIT new line add and commit the same
+	// GIT new line add and commit the same
+	// GIT new line add and commit the same
 	@Test
 	public void Test12() {
 		System.out.println("Test12");
 	}
 
-	@Test //(groups = "Smoke Testing")
+	@Test // (groups = "Smoke Testing")
 	public void Test13() {
 		System.out.println("Test13");
 	}
